811b5a5786ed86982f88bb7af97b7d9bf1e4b345
fengruyi <646018623@qq.com>
2015-01-12 20:34:36 +0800

    添加 代码

--- src/com/android/smarthome_phone/BaseActivity.java
+++ src/com/android/smarthome_phone/BaseActivity.java
@@ -1,9 +1,11 @@
 package com.android.smarthome_phone;
 
-import com.fengruyi.common.util.AppManager;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.fengruyi.common.util.AppManager;
 /**
  * 应用基类activity，其他的需要继承，方便管理
  * @author fengruyi
@@ -15,13 +17,38 @@ public class BaseActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		overridePendingTransition(R.anim.slide_in_right, R.anim.retain_);//以从右往左的动画进入这个页面
 		AppManager.getAppManager().addActivity(this);
 	}
 	
+	@Override
+	public void finish() {
+		super.finish();
+		overridePendingTransition(0, R.anim.slide_out_left);//退出页面时从右往左动画退出
+	}
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		AppManager.getAppManager().finishActivity(this);
+	}
+	public void startActivity(Class<? extends Activity> className){
+		Intent intent = new Intent(this,className);
+		startActivity(intent);
+	}
+	/**
+	 * 通用设备标题的方法
+	 * @param str
+	 */
+	public void setBarTitle(String str){
+		((TextView)findViewById(R.id.bar_title)).setText(str);
+	}
+	
+	/**
+	 * 通用设备标题的方法
+	 * @param resId
+	 */
+	public void setBarTitle(int resId){
+		((TextView)findViewById(R.id.bar_title)).setText(resId);
 	}
 }
--- src/com/android/smarthome_phone/ui/SpalishActivity.java
+++ src/com/android/smarthome_phone/ui/SpalishActivity.java
@@ -1,22 +1,19 @@
 package com.android.smarthome_phone.ui;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-
-
-
-
-
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.android.smarthome_phone.BaseActivity;
+import com.android.smarthome_phone.MyApplicatioin;
 import com.android.smarthome_phone.R;
 import com.android.smarthome_phone.entity.LightInfo;
 import com.android.smarthome_phone.entity.Lights;
 import com.fengruyi.common.log.Logger;
+import com.fengruyi.common.util.PrefsUtil;
+import com.fengruyi.common.util.TextUtil;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
@@ -33,44 +30,75 @@ public class SpalishActivity extends BaseActivity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//		//从assets中解释xml成对象
-			XStream	xstream = new XStream(new DomDriver()); 
-			xstream.alias("lights", Lights.class);//必须加，否则默认是含有包全全路径的名称
-			xstream.alias("lightinfo", LightInfo.class);
-			LightInfo info = new LightInfo();
-			info.setBrightness_control(false);
-			info.setLight_name("灯光1");
-			info.setLight_id(1);
-			
-			LightInfo info2 = new LightInfo();
-			info2.setBrightness_control(false);
-			info2.setLight_name("灯光2");
-			info2.setLight_id(2);
-			List<LightInfo> infos = new ArrayList<LightInfo>();
-			infos.add(info);
-			infos.add(info2);
-			Lights lights = new Lights();
-			lights.setLights_list(infos);
-			String xmlStr = xstream.toXML(lights); 
-			Logger.e("SpalishActivity", "xmlStr-->"+xmlStr);
-			
-			
-			List<LightInfo> list;
-			
-			try {
-				Lights lights_ =  (Lights) xstream.fromXML(getAssets().open("lightinfo.xml"));
+//		 	LoadingDialog.showLoadingDialog(this, "正在配置信息");
+////		//从assets中解释xml成对象
+//			XStream	xstream = new XStream(new DomDriver()); 
+//			xstream.alias("lights", Lights.class);//必须加，否则默认是含有包全全路径的名称
+//			xstream.alias("lightinfo", LightInfo.class);
+//			LightInfo info = new LightInfo();
+//			info.setBrightness_control(false);
+//			info.setLight_name("灯光1");
+//			info.setLight_id(1);
+//			
+//			LightInfo info2 = new LightInfo();
+//			info2.setBrightness_control(false);
+//			info2.setLight_name("灯光2");
+//			info2.setLight_id(2);
+//			List<LightInfo> infos = new ArrayList<LightInfo>();
+//			infos.add(info);
+//			infos.add(info2);
+//			Lights lights = new Lights();
+//			lights.setLights_list(infos);
+//			String xmlStr = xstream.toXML(lights); 
+//			Logger.e("SpalishActivity", "xmlStr-->"+xmlStr);
+//			
+//			
+//			List<LightInfo> list;
+//			
+//			try {
+//				Lights lights_ =  (Lights) xstream.fromXML(getAssets().open("lightinfo.xml"));
+//				 Logger.e("SpalishActivity", "list-->"+lights_);
+//				for (LightInfo lightInfo : lights_.getLights_list()) {
+//					Logger.e("SpalishActivity", "lightInfo"+lightInfo.toString());
+//				}
+//			} catch (Exception e) {
+//				 Logger.e("SpalishActivity", e.toString());
+//				e.printStackTrace();
+//			}
+			String json = PrefsUtil.getInstance().getString("deviceinfo");
+			if(TextUtil.isEmpty(json)){
+				try {
+				 Logger.e("SpalishActivity", "xml 解释");
+				 XStream	xstream = new XStream(new DomDriver()); 
+				 Lights lights_ =  (Lights) xstream.fromXML(getAssets().open("lightinfo.xml"));
+				 MyApplicatioin.getInstance().setmDevice(lights_);
+				 PrefsUtil.getInstance().putString("deviceinfo", JSON.toJSONString(lights_));
 				 Logger.e("SpalishActivity", "list-->"+lights_);
 				for (LightInfo lightInfo : lights_.getLights_list()) {
 					Logger.e("SpalishActivity", "lightInfo"+lightInfo.toString());
 				}
-			} catch (Exception e) {
-				 Logger.e("SpalishActivity", e.toString());
-				e.printStackTrace();
+				} catch (Exception e) {
+					 Logger.e("SpalishActivity", e.toString());
+					e.printStackTrace();
+				}
+				
+			}else{
+				 Logger.e("SpalishActivity", "JSOn 解释");
+				 Lights lights_ = JSON.parseObject(json, Lights.class);
+				MyApplicatioin.getInstance().setmDevice(lights_);
 			}
-			
+			Handler handler = new Handler();
+			handler.postDelayed(new Runnable() {
+				
+				@Override
+				public void run() {
+					Intent intent = new Intent(SpalishActivity.this,MainActivity.class);
+					startActivity(intent);
+					
+				}
+			},2*1000);
 			
 
-		
 	
 	}
 	
